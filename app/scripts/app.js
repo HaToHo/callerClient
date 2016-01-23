0a89ef79c26be4661943f1b7c3059bd9d613c529
Hans <hans@sektor.se>
2016-01-24 00:14:17 +0100

    Got it all up and running. Lets get travis up and running as well. :)

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name callerClientApp
+ * @description
+ * # callerClientApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('callerClientApp', [
+    'ngAnimate'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name callerClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the callerClientApp
+ */
+angular.module('callerClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
